5734b3205301fbb20ba58097afde3c89fc0fbaa3
Pari-Bansal-2004 <paribnsl1310@gmail.com>
2026-06-18 12:36:01 +0530

    remove arcjet middleware

--- middleware.tsx
+++ middleware.tsx
@@ -1,4 +1,4 @@
-import arcjet, { createMiddleware, detectBot, shield } from '@arcjet/next';
+// import arcjet, { createMiddleware, detectBot, shield } from '@arcjet/next';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
@@ -7,23 +7,31 @@ const isProtectedRoute = createRouteMatcher([
     "/transaction(.*)",
 ]);
 
-const aj= arcjet({
-  key: process.env.ARCJET_KEY!,
-  rules:[
-    shield({
-      mode:'LIVE'
-    }),
-    detectBot({
-      mode:'LIVE',
-      allow:[
-        "CATEGORY:SEARCH_ENGINE",
-        "GO_HTTP"
-      ]
-    })
-  ]
-});
+// const aj= arcjet({
+//   key: process.env.ARCJET_KEY!,
+//   rules:[
+//     shield({
+//       mode:'LIVE'
+//     }),
+//     detectBot({
+//       mode:'LIVE',
+//       allow:[
+//         "CATEGORY:SEARCH_ENGINE",
+//         "GO_HTTP"
+//       ]
+//     })
+//   ]
+// });
 
-const clerk =  clerkMiddleware(async(auth, req)=>{
+// const clerk =  clerkMiddleware(async(auth, req)=>{
+//     if(!isProtectedRoute(req)) return;
+    
+//     const { userId, redirectToSignIn } = await auth();
+//     if(!userId) {
+//         return redirectToSignIn();
+//     }
+// });
+export default clerkMiddleware(async(auth, req)=>{
     if(!isProtectedRoute(req)) return;
     
     const { userId, redirectToSignIn } = await auth();
@@ -32,7 +40,7 @@ const clerk =  clerkMiddleware(async(auth, req)=>{
     }
 });
 
-export default createMiddleware(aj, clerk);
+// export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
